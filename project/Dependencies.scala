25943433be185b262980b7cf627fe5f47011bd5c
Ahmad Ragab <Averroes2006@gmail.com>
2023-11-18 22:59:34 -0500

    cumulative changes

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object V {
-  lazy val zio     = "2.0.16"
+  lazy val zio     = "2.0.19"
   lazy val zioCats = "23.0.0.8"
   lazy val sttp    = "3.9.0"
   lazy val circe   = "0.14.6"
@@ -9,7 +9,7 @@ object V {
 
 object Dependencies {
   lazy val cats              = "org.typelevel" %% "cats-core"                     % "2.10.0"
-  lazy val catsEffect        = "org.typelevel" %% "cats-effect"                   % "3.5.1"
+  lazy val catsEffect        = "org.typelevel" %% "cats-effect"                   % "3.5.2"
   lazy val catsEffectTesting = "org.typelevel" %% "cats-effect-testing-scalatest" % "1.5.0" % Test
 
   lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % "3.2.17" % Test
@@ -28,7 +28,7 @@ object Dependencies {
   lazy val zio        = "dev.zio" %% "zio"               % V.zio
   lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt"      % V.zio
   lazy val zioCats    = "dev.zio" %% "zio-interop-cats"  % V.zioCats
-  lazy val zioSlf4j   = "dev.zio" %% "zio-logging-slf4j" % "2.1.14"
+  lazy val zioSlf4j   = "dev.zio" %% "zio-logging-slf4j" % "2.1.15"
 
   lazy val loggingFrontend = "ch.qos.logback" % "logback-classic" % "1.4.11"
 
