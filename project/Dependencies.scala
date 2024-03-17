0af05db392e3e165f7f25e31ce7e056f6269ee76
Ahmad Ragab <Averroes2006@gmail.com>
2024-03-17 17:22:13 -0400

    Scala Steward Changes Cumulative 03/17/2024 (#66)
    * Update cats-effect to 3.5.4 in steward (#65)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update async-http-client-backend-zio, ... to 3.9.4 in steward (#64)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update scalatest to 3.2.18 in steward (#56)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update scala3-library to 3.3.3 in steward (#62)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update scalafmt-core to 3.8.0 in steward (#60)
    
    * Update scalafmt-core to 3.8.0 in steward
    
    * Reformat with scalafmt 3.8.0
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.8.0' to .git-blame-ignore-revs
    
    ---------
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update sbt to 1.9.9 in steward (#59)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update zio-logging-slf4j to 2.1.17 in steward (#53)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    Co-authored-by: Ahmad Ragab <Averroes2006@gmail.com>
    
    * Update paradox-material-theme, ... to 0.7.0 in steward (#58)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    
    * Update logback-classic to 1.5.3 in steward (#63)
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>
    Co-authored-by: Ahmad Ragab <Averroes2006@gmail.com>
    
    ---------
    
    Co-authored-by: scala-steward-asragab[bot] <142696383+scala-steward-asragab[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,16 @@ import sbt.*
 object V {
   lazy val zio     = "2.0.21"
   lazy val zioCats = "23.0.0.8"
-  lazy val sttp    = "3.9.0"
+  lazy val sttp    = "3.9.4"
   lazy val circe   = "0.14.6"
 }
 
 object Dependencies {
   lazy val cats              = "org.typelevel" %% "cats-core"                     % "2.10.0"
-  lazy val catsEffect        = "org.typelevel" %% "cats-effect"                   % "3.5.2"
+  lazy val catsEffect        = "org.typelevel" %% "cats-effect"                   % "3.5.4"
   lazy val catsEffectTesting = "org.typelevel" %% "cats-effect-testing-scalatest" % "1.5.0" % Test
 
-  lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % "3.2.17" % Test
+  lazy val scalaTest  = "org.scalatest"  %% "scalatest"  % "3.2.18" % Test
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0" % Test
 
   lazy val sttpFs2   = "com.softwaremill.sttp.client3" %% "fs2"                           % V.sttp
@@ -24,12 +24,12 @@ object Dependencies {
   lazy val circeGeneric = "io.circe" %% "circe-generic" % V.circe
   lazy val circeParser  = "io.circe" %% "circe-parser"  % V.circe
 
-  lazy val zioTest    = "dev.zio" %% "zio-test"          % V.zio
-  lazy val zio        = "dev.zio" %% "zio"               % V.zio
-  lazy val zioTestSbt = "dev.zio" %% "zio-test-sbt"      % V.zio
-  lazy val zioCats    = "dev.zio" %% "zio-interop-cats"  % V.zioCats
-  lazy val zioSlf4j   = "dev.zio" %% "zio-logging-slf4j" % "2.1.16"
-  lazy val loggingFrontend = "ch.qos.logback" % "logback-classic" % "1.4.14"
+  lazy val zioTest         = "dev.zio"       %% "zio-test"          % V.zio
+  lazy val zio             = "dev.zio"       %% "zio"               % V.zio
+  lazy val zioTestSbt      = "dev.zio"       %% "zio-test-sbt"      % V.zio
+  lazy val zioCats         = "dev.zio"       %% "zio-interop-cats"  % V.zioCats
+  lazy val loggingFrontend = "ch.qos.logback" % "logback-classic"   % "1.5.3"
+  lazy val zioSlf4j        = "dev.zio"       %% "zio-logging-slf4j" % "2.1.17"
 
   lazy val core: Seq[ModuleID] = Seq(
     cats,
